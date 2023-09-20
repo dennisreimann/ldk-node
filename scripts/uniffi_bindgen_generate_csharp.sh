8c4d919021ed0cf7351f1fe5cfd0d48b72636ee6
Dennis Reimann <mail@dennisreimann.de>
2023-09-20 18:56:28 +0200

    C#: Update bindings and script

--- scripts/uniffi_bindgen_generate_csharp.sh
+++ scripts/uniffi_bindgen_generate_csharp.sh
@@ -6,4 +6,8 @@ mkdir -p $BINDINGS_DIR
 
 cargo build --release --features uniffi || exit 1
 uniffi-bindgen-cs bindings/ldk_node.udl -o "$BINDINGS_DIR" || exit 1
-cp ./target/release/libldk_node.{a,so} "$BINDINGS_DIR" || exit 1
+
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+  mkdir -p "$BINDINGS_DIR"/x86_64/
+  cp ./target/release/libldk_node.so "$BINDINGS_DIR"/x86_64/ || exit 1
+fi
